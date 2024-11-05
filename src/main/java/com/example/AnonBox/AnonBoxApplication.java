adbf398deda3d79adda17fd5adb04bf6c23f7151
Eder Sparenberger <edsparen@gmail.com>
2024-11-05 18:55:43 -0300

    Initialize AnonBox project with basic structure and configuration files

---
+++ src/main/java/com/example/AnonBox/AnonBoxApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnonBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AnonBox/AnonBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnonBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
